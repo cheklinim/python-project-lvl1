53cb5e31e9bc839f3e07183722c33fdae5f9ccf7
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2023-05-06 21:36:16 +0300

    Linter errors fixed

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,13 @@
+"""Module for CLI interface."""
+
 import prompt
 
 
 def welcome_user():
+    """Greet a user."""
     name = prompt.string('May I have your name?\n')
-    print(f"Hello, {name}!")
+    print(f'Hello, {name}!')
 
 
-if __name__ == "__main__":
-    print("This file have to be imported!")
-
+if __name__ == '__main__':
+    print('This file have to be imported!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+"""Main brain-games module."""
+
 from brain_games.cli import welcome_user
 
+
 def main():
-    print("Welcome to the Brain Games!")
+    """Start the game."""
+    print('Welcome to the Brain Games!')
     welcome_user()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
