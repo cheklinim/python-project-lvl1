b2ab0d1d3711ab7f4cbfa9e801a833d6e33fbf50
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2023-05-08 19:51:59 +0300

    Created lib folder with calculatons.py

---
+++ brain_games/lib/calculations.py
@@ -0,0 +1,12 @@
+"""Calculations library module."""
+
+def is_even(x):
+    return not(x % 2)
+
+
+def main():
+    print('This is library module. It should be imported')
+
+
+if __name__ == '__main__':
+    main()
