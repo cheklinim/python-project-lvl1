a0b912ca17ee2dc0b30c6980549fff9ff3052780
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2023-06-28 22:18:45 +0300

    gcd function realized in calculations.py

--- brain_games/lib/calculations.py
+++ brain_games/lib/calculations.py
@@ -5,6 +5,15 @@ def is_even(x):
     return not (x % 2)
 
 
+def gcd(a, b):
+    while a * b != 0:
+        if a > b:
+            a %= b
+        else:
+            b %= a
+    return (a + b)
+
+
 def main():
     print('This is library module. It should be imported')
 
