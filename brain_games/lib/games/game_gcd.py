a5cd7f241ef4721a4f01a9bb772827cd3ec41412
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2023-07-02 17:11:31 +0300

    game_gcd added

---
+++ brain_games/lib/games/game_gcd.py
@@ -0,0 +1,28 @@
+"""Module with main logic of game-calc."""
+
+import prompt
+import random
+from brain_games.lib.greeting import greet_user
+from brain_games.lib.answer_examenation import answer_exam
+from brain_games.lib.calculations import gcd
+
+
+def game_gcd():
+    name = greet_user()
+    print('Find the greatest common divisor of given numbers.')
+    counter = 0
+    while counter < 3:
+        num1 = random.randint(1, 99)
+        num2 = random.randint(1, 99)
+        answer = gcd(num1, num2)
+        print(f'Question: {num1} {num2}')
+        user_answer = int(prompt.string('Your answer:'))
+        counter = answer_exam(answer, user_answer, counter, name)
+
+
+def main():
+    print('This is library file. It should be imported.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+"""The game about simple calculations"""
+
+from brain_games.lib.games.game_gcd import game_gcd
+
+
+def main():
+    """Main function of module"""
+    game_gcd()
+
+
+if __name__ == '__main__':
+    main()
